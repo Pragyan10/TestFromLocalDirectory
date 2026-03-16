ec4d369ca803264fb9cbd1e199264e5ba4637687
Pragyan10 <pragyankc1@gmail.com>
2026-03-16 14:58:42 -0400

    working with branch

---
+++ AmazonAlbum.java
@@ -0,0 +1,29 @@
+public class AmazonAlbum {
+    private String name;
+    private final Song[] songs;
+    private final int numberOfSongs;
+
+    public AmazonAlbum(String name, Song[] songs, int numberOfSongs) {
+        this.name = name;
+        this.songs = songs;
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Album", name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Song[] getSongs() {
+        return songs;
+    }
+
+    // Optional helper (not required, but useful)
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+}
